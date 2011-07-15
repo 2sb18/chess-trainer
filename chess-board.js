89f056284aabb62db6d165a6c08715f8f60a3931
Steve Bragg <steve@empresseffects.com>
2011-07-15 16:35:38 -0400

    - add functions for moving adding pieces, removing pieces, and moving pieces

--- chess-board.js
+++ chess-board.js
@@ -2,23 +2,20 @@
 var dark_color = 'brown';
 var light_color = 'rgb(230,220,230)';
 var size_of_board = 0.95;
+var speed_of_move = 400;
 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
-// initial pieces, get rid of this later, we shouldn't need it.
-// the pieces are kept in the user interface as img elements
-var pieces = [  {piece:"wb", square:"a4"},
-                {piece:"bb", square:"a5"}
-];
 
 // selected_piece is a jQuery object with the selected piece in it.
 var selected_piece;
 
 var dimensions = {};
 
+var board;
+
 function build_board() {
   $('body').append("<div id='board'></div>");
-  var board = $('#board');
+  board = $('#board');
   for (var i in letters) {
     for (var j = 1; j <= 8; j++) {
       if ((Number(i) + j) % 2 === 1) {
@@ -27,10 +24,6 @@ function build_board() {
         board.append("<div class='square' id='" + letters[i] + j + "' style='background-color:" + light_color + ";position:absolute' />");
       }
     }
-  }
-  for (var i in pieces) {
-    board.append("<img class='piece' id='" + pieces[i].square + "' src='./img/" + pieces[i].piece + ".png' style='position:absolute' " +
-      "ondragstart='return false' onselectstart='return false'/>");
   }
   
   resize_and_move_board();
@@ -69,7 +62,36 @@ function resize_and_move_board() {
   });
 }
 
-build_board(); 
+// example input is square = 'a4'
+function remove_piece(square) {
+  $('.piece#' + square).remove();
+}
+
+// example: {piece: "bb", square: "a4"}
+function add_piece(piece, square) {
+  // if there's a piece already there, remove it
+  remove_piece(square);
+  board.append("<img class='piece' id='" + square + "' src='./img/" + piece + ".png' style='position:absolute' " +
+    "ondragstart='return false' onselectstart='return false'/>");
+  var jQuerySquare = $('.piece#' + square);
+  jQuerySquare.css(get_position_and_size_from_square(jQuerySquare.attr('id')));
+}
+
+function move_piece(from_square, to_square) {
+  // get the piece that's being moved
+  var piece = $(".piece#" + from_square);
+  if (piece.length === 0) {
+    return;
+  }
+  var piece_to_remove = $('.piece#' + to_square);
+  piece.attr("id", to_square);
+  piece.animate(get_position_and_size_from_square(to_square), {
+    duration: speed_of_move, 
+    complete: function() {
+      piece_to_remove.remove(); // remove any pieces on the destination square 
+    }
+  });
+}
 
 // events
 
@@ -92,6 +114,8 @@ function drop_selected_piece(e) {
   // find out what square mouse is on
   var dropped_square = get_square_from_mouse(e);
   if (dropped_square !== undefined) {
+    // remove a piece that is on this square
+    remove_piece(dropped_square);
     // set the position of the selected piece to the position of the square it's on
     selected_square = $(".square#" + dropped_square);
     // change the id of the selected_piece to match the square it's being dropped on.
@@ -104,8 +128,6 @@ function drop_selected_piece(e) {
   selected_piece = undefined;
 }
   
-  
-
 $('.piece').live("mousedown", function(e) {
   selected_piece = $(this);
   selected_piece.css("z-index", "2");
@@ -115,3 +137,14 @@ $('.piece').live("mousedown", function(e) {
 $('#board').live("mousemove", function(e) {
   selected_piece_to_mouse(e);
 });
+
+// actions
+
+build_board(); 
+add_piece("bb", "a1");
+add_piece("wb", "e4");
+add_piece("wk", "g8");
+add_piece("bp", "a2");
+move_piece("a1", "g8");
+move_piece("e4", "a2");
+
