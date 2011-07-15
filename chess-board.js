ed48a0fc1980af0075cf1122b1408eafbfcab084
Steve Bragg <steve@empresseffects.com>
2011-07-15 11:02:42 -0400

    - pieces show up now.

--- chess-board.js
+++ chess-board.js
@@ -1,10 +1,16 @@
+//configuration
+var dark_color = 'rgb(110, 110, 110)';
+var light_color = 'rgb(200, 230, 230)';
+var size_of_board = 0.95;
+
 var board;
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
-var pieces = [{piece:"B", square:"a4"}];
+var pieces = [  {piece:"wb", square:"a4"},
+                {piece:"bb", square:"a5"}
+];
 
-var dark_color = 'black';
-var light_color = 'gray';
+
 
 function build_board() {
   $('body').append("<div id='board'></div>");
@@ -18,33 +24,35 @@ function build_board() {
       }
     }
   }
+  for (var i in pieces) {
+    board.append("<img class='piece' id='" + pieces[i].square + "' src='./img/" + pieces[i].piece + ".png' style='position:absolute' />");
+  }
+  
   resize_and_move_board();
 }
   
   
 function resize_and_move_board() {
-  var result = {};
   var smaller_dimension = $(window).height() > $(window).width() ? $(window).width() : $(window).height();
-  var board_width = Math.round(smaller_dimension * 0.8);
+  var board_width = Math.round(smaller_dimension * size_of_board);
   var square_width = Math.round(board_width / 8);
   var board_top = Math.round($(window).height() / 2 - board_width / 2);
   var board_left = Math.round($(window).width() / 2 - board_width / 2);
   
-  // takes something like 'g4' and returns an object with top and left
-  function set_css_on_square (jQuerySquare) {
-    var algebraic = jQuerySquare.attr('id');
+  
+  function get_position_and_size_from_algebraic (algebraic) {
+    result = {};
     result.left = board_left + letters.indexOf(algebraic.charAt(0)) * square_width;
     result.top = board_top + (8 - Number(algebraic.charAt(1))) * square_width;
     result.width = square_width;
     result.height = square_width;
-    jQuerySquare.css(result);
+    return result;
   }
   
-  $('.square').each(function(index, element) {
-    set_css_on_square($(this));
+  $('.square, .piece').each(function(index, element) {
+    var jQuerySquare = $(this);
+    jQuerySquare.css(get_position_and_size_from_algebraic(jQuerySquare.attr('id')));
   });
-  
-  
 }
 
 build_board(); 
