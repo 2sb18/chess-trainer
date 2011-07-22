a1f2633e17558a4803c86662c447a9fd5f6c072c
Steve Bragg <steve@empresseffects.com>
2011-07-22 14:32:31 -0400

    - got the basic draw_arrows() written. Have to add arrow heads at some point in the future.

--- chess-board.js
+++ chess-board.js
@@ -11,15 +11,22 @@ var ChessBoard = function(move_function) {
   var light_color = 'rgb(230,220,230)';
   var selected_square_color = 'rgb(27, 119, 224)';
   var size_of_board = 0.95;
-  var speed_of_move = 0;
+  var thicknes_of_arrow = 0.2;  // relative to the width of the squares
+  var z_square = -2;
+  var z_piece  = -1;
+  var z_canvas = 1;
 
   var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
   var selected_square;
 
   var dimensions = {};
+  
+  var canvas_context;
 
   var board;
+  
+  var stored_arrows = [];
   
   // body is a jQuery object containing the body.
   function build_board() {
@@ -30,11 +37,9 @@ var ChessBoard = function(move_function) {
         var square = letters[i] + j;
         board.append("<img class='square' id='" + square + "' src='./img/b.gif' />");
         set_background_color_to_default(square);
-        $('.square#' + square).css("position", "absolute").css("z-index", "-2");
+        $('.square#' + square).css("position", "absolute").css("z-index", z_square);
       }
     }
-    // create arrow_canvas
-    $('body').append("<canvas id='arrow_canvas'></div>");
     resize_and_move_board();
   }
 
@@ -54,9 +59,6 @@ var ChessBoard = function(move_function) {
     result.width = dimensions.square_width;
     result.height = dimensions.square_width;
     return result;
-  }
-  
-  function get_center_of_square (square) {
   }
 
   // takes an event object e and returns the square like 'a4'
@@ -81,8 +83,14 @@ var ChessBoard = function(move_function) {
       jQuerySquare.css(get_position_and_size_from_square(jQuerySquare.attr('id')));
     });
     
-    // resize arrow canvas
-    $('#arrow_canvas').width(board_width).height(board_width).offset({top:dimensions.board_top, left:dimensions.board_left});
+    // remove old canvas, and create new one based on the new dimensions.
+    $('canvas#arrow_canvas').remove();
+    $('body').append("<canvas id='arrow_canvas' width='" + board_width + "' height='" + board_width + "'></div>");
+    $('canvas#arrow_canvas').offset({left:dimensions.board_left, top:dimensions.board_top}).css("z-index", z_canvas);
+    canvas_context = $('canvas#arrow_canvas').get(0).getContext('2d');
+    
+    // redraw the canvas
+    draw_arrows(stored_arrows); 
   }
 
   // example: {piece: "bb", square: "a4"}
@@ -98,7 +106,7 @@ var ChessBoard = function(move_function) {
         remove_piece(square);
       }
     }
-    board.append("<img class='piece' id='" + square + "' src='./img/" + piece + ".png' style='position:absolute;z-index:-1' " +
+    board.append("<img class='piece' id='" + square + "' src='./img/" + piece + ".png' style='position:absolute;z-index:" + z_piece + "' " +
       "ondragstart='return false' onselectstart='return false'/>");
     var jQuerySquare = $('.piece#' + square);
     jQuerySquare.css(get_position_and_size_from_square(jQuerySquare.attr('id')));
@@ -113,7 +121,47 @@ var ChessBoard = function(move_function) {
       $('.piece#' + square).remove();
     }
   }
+  
+  // arrows is an array of arrows
+  // an arrow consists of an object, {from, to, color}
+  function draw_arrows (arrows) {
+  
+  
+    arrows = [{from:'e2', to:'e4'}, {from:'d2', to:'d4'}, {from:'b1', to:'c3'}, {from:'g1', to:'f3'}];
+  
+    stored_arrows = arrows;
+  
+  
+    // this is with respect to the board
+    function get_center_of_square (square) {
+      var result = {};
+      result.top = (8 - Number(square.charAt(1))) * dimensions.square_width + dimensions.square_width/2;
+      result.left = letters.indexOf(square.charAt(0)) * dimensions.square_width + dimensions.square_width/2;
+      return result;
+    }
+  
+    // get rid of arrows already on screen
+    // setting the width of a canvas erases everything on it
+    var canvas_element = $('canvas#arrow_canvas').get(0);
+    canvas_element.width--;
+    canvas_element.width++;
 
+  
+    canvas_context.strokeStyle = "rgba(100,100,100,0.5)";
+    canvas_context.lineWidth = Math.round(dimensions.square_width * thicknes_of_arrow);
+    
+    for (var k in arrows) {
+      var from = get_center_of_square (arrows[k].from);
+      var to   = get_center_of_square (arrows[k].to);
+      canvas_context.moveTo(from.left, from.top);
+      canvas_context.lineTo(to.left, to.top);
+    }
+    //canvas_context.moveTo(0, 0);
+    //canvas_context.lineTo(65, 65);
+    canvas_context.stroke();
+    return canvas_context;
+  }
+ 
   // events
 
   $(window).resize(resize_and_move_board);
@@ -141,9 +189,6 @@ var ChessBoard = function(move_function) {
     } else {
       // move piece to new square
       move_function({from: selected_square, to: square});
-      //if (move_result !== null) {   // the move_verification_function will update the chess-board
-      //  move_piece(selected_square, square);
-      //}
       deselect_square();
     }
   });
@@ -161,6 +206,9 @@ var ChessBoard = function(move_function) {
     },
     draw_arrows: function (arrows) {
       return draw_arrows (arrows);
+    },
+    get_dimensions: function () {
+      return dimensions;
     }
   };
   
--- chess-trainer.js
+++ chess-trainer.js
@@ -1,37 +1,38 @@
 // chess-trainer glues together the chess.js and chess-board.js
 
-function sync_board_to_engine (chess_engine, chess_board) {
-  for (var i in chess_engine.SQUARES) {
-    var piece = chess_engine.get(chess_engine.SQUARES[i]);
+function sync_board_to_engine (engine, board) {
+  for (var i in engine.SQUARES) {
+    var piece = engine.get(engine.SQUARES[i]);
     if (piece === null) {
-      chess_board.remove_piece(chess_engine.SQUARES[i]);
+      board.remove_piece(engine.SQUARES[i]);
     } else {
-      chess_board.add_piece(piece.color + piece.type, chess_engine.SQUARES[i]);
+      board.add_piece(piece.color + piece.type, engine.SQUARES[i]);
     }
   }
 }
 
-var chess_engine = new Chess();
+var engine = new Chess();
 
-var chess_tree = new ChessTree();
+var tree = new ChessTree();
 
 function move_function (move_object) {
-  var move = chess_engine.move(move_object);
+  var move = engine.move(move_object);
   if (move !== null) {
-    chess_tree.moveTo(move.san);
-    sync_board_to_engine (chess_engine, chess_board);
+    tree.moveTo(move.san);
+    sync_board_to_engine (engine, board);
   }
 }
      
-var chess_board = new ChessBoard(move_function);
-chess_board.build_board();
+var board = new ChessBoard(move_function);
+board.build_board();
+board.draw_arrows();
 
-sync_board_to_engine (chess_engine, chess_board);
+sync_board_to_engine (engine, board);
 
 $(document).keypress (function (e) {
   if (e.keyCode === 37) {   // left arrow button
-    chess_engine.undo();
-    chess_tree.moveBack();
-    sync_board_to_engine (chess_engine, chess_board);
+    engine.undo();
+    tree.moveBack();
+    sync_board_to_engine (engine, board);
   }
 });
